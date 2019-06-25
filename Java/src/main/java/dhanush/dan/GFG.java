28a98995972f6e13a9952d63c901554159ce8f1c
katuridhanushkasrinivas <katuridhanushkasrinivas@katuris-mbp.fios-router.home>
2019-06-24 23:33:09 -0400

    Add -GFG

---
+++ Java/src/main/java/dhanush/dan/GFG.java
@@ -0,0 +1,30 @@
+package dhanush.dan;
+
+public class GFG {
+	
+	static boolean isSubSequence(String str1, String str2, int m, int n) {
+		
+		int j=0;
+		
+		for(int i =0; i<n && j <m; i++) 
+			if(str1.charAt(i)==str2.charAt(j))
+				j++;
+	
+		return (j==m);
+	
+
+}
+
+	public static void main(String[] args) {
+		String str1 = "gksrek";
+		String str2 = "geeksforgeeks";
+		int m = str1.length();
+		int n = str2.length();
+		
+		boolean res = isSubSequence(str1,str2,m,n);
+		if(res)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+	}
